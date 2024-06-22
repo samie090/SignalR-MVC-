72130764a5c4c28a36f6fea76a5436205fc331f5
samie090 <samie_090@yahoo.com>
2024-06-22 18:58:50 +0500

    Initialization

---
+++ SiganlRChatDemo/Hubs/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SiganlRChatDemo.Hubs
+{
+    public class ChatHub:Hub
+    {
+        private static Dictionary<string, string> connectedClients = new Dictionary<string, string>();
+        public async Task SendMessage(string username,string message) 
+        {
+            await Clients.All.SendAsync("ReceiveMessage", username, message);            
+        }
+        public async Task JoinChat(string user, string message)
+        {
+            connectedClients[Context.ConnectionId] = user;
+            await Clients.Others.SendAsync("ReceiveMessage", user, message);
+        }
+
+        private async Task LeaveChat()
+        {
+            if (connectedClients.TryGetValue(Context.ConnectionId, out string user))    //Will put First arg value(Not Key(coz key is 1st arg)) to 2nd arg-- Here if returs true if key is found n vice versa
+            {
+                var message = $"{user} left the chat";
+                await Clients.Others.SendAsync("ReceiveMessage", user, message);
+            }
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await LeaveChat();
+            await base.OnDisconnectedAsync(exception);
+
+        }
+    }
+}
---
+++ SiganlRChatDemo/Program.cs
@@ -0,0 +1,41 @@
+using SiganlRChatDemo.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:5500")
+                .AllowAnyHeader()
+                .WithMethods("GET", "POST")
+                .SetIsOriginAllowed((host) => true)
+                .AllowCredentials();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapHub<ChatHub>("/chathub");
+
+app.Run();
